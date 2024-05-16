791ad83af78e5728d9892ce1e5c1af4f2f167be6
= <=>
2024-05-16 11:17:55 +0000

    mise en place de l'interface d'inscription

--- pages/article.php
+++ pages/article.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    <?php
-        require_once './include/menu.php';
-    ?>
+<?php
+   require_once '../include/menu.php';
+   ?> 
 </body>
 </html>
